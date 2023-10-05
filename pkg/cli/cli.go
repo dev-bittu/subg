1d6c7469778fac4774804543ed6e0c38287d2f73
DevBittu <devbittu@proton.me>
2023-10-06 01:16:09 +0530

    introduce feature: check internet connection before starting, handle error while executing

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -2,8 +2,9 @@ package cli
 
 import (
 	"fmt"
-	"log"
-	"os"
+	"net"
+	"net/http"
+	"time"
 
 	"github.com/dev-bittu/subg/config"
 	"github.com/dev-bittu/subg/pkg/scanner"
@@ -11,26 +12,55 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-  Use:   config.Config["name"].(string),
-  Short: config.Config["title"].(string),
-  Long: config.Config["desc"].(string),
-  Version: config.Config["version"].(string),
-  Run: func(cmd *cobra.Command, args []string) {
-    if Wordlist == "" || Domain == "" {
-      fmt.Println("Enter Wordlist/Domain...")
-      return
-    }
-    scanr, err := scanner.NewScanner(Domain, Wordlist, Thread)
-    if err != nil {
-      fmt.Println(err)
-    }
-    scanr.Scan()
-  },
+	Use:     config.Config["name"].(string),
+	Short:   config.Config["title"].(string),
+	Long:    config.Config["desc"].(string),
+	Version: config.Config["version"].(string),
+	Run: func(cmd *cobra.Command, args []string) {
+		if !isConnectedToInternet() {
+			fmt.Println("You are not connected to internet.\nCheck your connection.")
+			return
+		}
+		scanr, err := scanner.NewScanner(Domain, Wordlist, Thread)
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = scanr.Scan()
+		if err != nil {
+			fmt.Println(err)
+		}
+	},
 }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    log.Fatalln(err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println("Solve the problem to move further:")
+		fmt.Println("Problem:", err)
+	}
+}
+
+func isConnectedToInternet() bool {
+	// Create a timeout for the request
+	timeout := time.Duration(2 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	// Make a GET request to a reliable server
+	resp, err := client.Get("https://www.example.com")
+	if err != nil {
+		// Check if the error is due to no internet connection
+		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+			return false
+		}
+		return false
+	}
+
+	defer resp.Body.Close()
+
+	// Check the response status code
+	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+		return true
+	}
+	return false
 }
