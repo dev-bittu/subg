778aca4bc9e2419dd477c85dcd8a72398d1a6231
DevBittu <devbittu@proton.me>
2023-10-06 01:17:39 +0530

    make wordlist and domain required, format structure

--- pkg/cli/flags.go
+++ pkg/cli/flags.go
@@ -1,15 +1,34 @@
 package cli
 
 var (
-  Wordlist string
-  Domain string
-  Thread int
+	Wordlist string
+	Domain   string
+	Thread   int
 )
 
 func init() {
-  rootCmd.PersistentFlags().StringVarP(&Domain, "domain", "d", "", "Domain to scan. Ex: example.com")
-  rootCmd.PersistentFlags().StringVarP(&Wordlist, "wordlist", "w", "", "Wordlist path (delimiter: newline or \\n). Ex: /usr/share/wdlist/wd.txt")
-  rootCmd.PersistentFlags().IntVarP(&Thread, "thread", "t", 100, "Thread (Default: 100) [subdomains/sec]")
+	rootCmd.PersistentFlags().StringVarP(
+		&Domain,
+		"domain",
+		"d",
+		"",
+		"Domain to scan. Example: example.com",
+	)
+	rootCmd.PersistentFlags().StringVarP(
+		&Wordlist,
+		"wordlist",
+		"w",
+		"",
+		"Wordlist path (delimiter: newline or \\n). Example: wdlist/subdomains.txt",
+	)
+	rootCmd.PersistentFlags().IntVarP(
+		&Thread,
+		"thread",
+		"t",
+		50,
+		"Thread [subdomains/sec]",
+	)
 
-  rootCmd.MarkFlagRequired("domain")
+	rootCmd.MarkPersistentFlagRequired("domain")
+	rootCmd.MarkPersistentFlagRequired("wordlist")
 }
