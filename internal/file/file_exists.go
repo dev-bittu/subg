864dc7c19f25a3f387504e75aecd09749b7662ee
DevBittu <devbittu@proton.me>
2023-10-06 01:13:12 +0530

    introduce file exists function

--- internal/file/file_exists.go
+++ internal/file/file_exists.go
@@ -6,11 +6,11 @@ import (
 )
 
 func IsFileExists(file string) (bool, error) {
-  _, err := os.Stat(file)
-  if err == nil {
-    return true, nil
-  } else if errors.Is(err, os.ErrNotExist) {
-    return false, nil
-  } 
-  return false, err
+	_, err := os.Stat(file)
+	if err == nil {
+		return true, nil
+	} else if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
 }
